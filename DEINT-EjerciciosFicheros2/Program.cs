e7eb7b5b64032556df9584ece7ff8694e7175f75
MJ-Curtido <manu17pc@gmail.com>
2022-11-16 10:14:45 +0100

    Agregar archivos de proyecto.

---
+++ DEINT-EjerciciosFicheros2/Program.cs
@@ -0,0 +1,34 @@
+﻿//C:\Users\Dam\Downloads\prueba
+String ruta1 = "";
+String nomFichero = "";
+String[] ficheros;
+String rutaFichero = "";
+
+
+Console.WriteLine("-------1-------");
+Console.WriteLine("Introduzca una ruta");
+ruta1 = Console.ReadLine();
+
+DateTime fechaMin = DateTime.MinValue;
+
+if (Directory.Exists(ruta1))
+{
+	if (Directory.GetFiles(ruta1).Length > 0)
+	{
+		ficheros = Directory.GetFiles(ruta1);
+
+		foreach (String file in ficheros)
+		{
+            //combinar rutas
+
+        }
+	}
+	else
+	{
+        Console.WriteLine("No hay ficheros dentro de la carpeta introducida.");
+    }
+}
+else
+{
+    Console.WriteLine("La ruta introducida no existe.");
+}
