8f65774f6bd934b424dc0e950aa316dd5dae91e0
Mxnuu7_ <manu17pc@gmail.com>
2022-11-20 22:01:45 +0100

    Fin

--- DEINT-EjerciciosFicheros2/Program.cs
+++ DEINT-EjerciciosFicheros2/Program.cs
@@ -1,7 +1,8 @@
 ﻿//C:\Users\Dam\Downloads\prueba
+//C:\Users\manu1\Downloads\prueba
 using System.Text.RegularExpressions;
 
-String ruta1 = "";
+String ruta1 = "C:\\Users\\Dam\\Downloads\\prueba";
 String rutaDirectorio = "";
 String nomFichero = "";
 String nomCarpeta = "";
@@ -10,7 +11,6 @@ String[] rutaSeparada;
 String extension = "";
 String rutaFichero = "";
 
-/*
 Console.WriteLine("-------1-------");
 Console.WriteLine("Introduzca una ruta");
 ruta1 = Console.ReadLine();
@@ -61,13 +61,20 @@ if (Directory.Exists(ruta1))
         Console.WriteLine("Escriba una extensión:");
         extension = Console.ReadLine();
 
-        foreach (String file in ficheros)
+        if (extension[0].Equals("."))
         {
-            if (extension.ToLower().Equals(Path.GetExtension(file).ToLower()))
+            foreach (String file in ficheros)
             {
-                Console.WriteLine(file);
-            }
+                if (extension.ToLower().Equals(Path.GetExtension(file).ToLower()))
+                {
+                    Console.WriteLine(file);
+                }
             
+            }
+        }
+        else
+        {
+            Console.WriteLine("La extensión tiene que empezar por un punto.");
         }
     }
     else
@@ -106,30 +113,37 @@ if (Directory.Exists(ruta1))
         Console.WriteLine("Escriba una extensión:");
         extension = Console.ReadLine();
 
-        foreach (String file in ficheros)
+        if (extension[0].Equals("."))
         {
-            if (extension.ToLower().Equals(Path.GetExtension(file).ToLower()))
+            foreach (String file in ficheros)
             {
-                rutaSeparada = file.Split("\\");
+                if (extension.ToLower().Equals(Path.GetExtension(file).ToLower()))
+                {
+                    rutaSeparada = file.Split("\\");
 
-                nomFichero = rutaSeparada[rutaSeparada.Length - 1];
+                    nomFichero = rutaSeparada[rutaSeparada.Length - 1];
 
-                rutaFichero = Path.Combine(rutaDirectorio, nomFichero);
+                    rutaFichero = Path.Combine(rutaDirectorio, nomFichero);
 
-                File.Copy(file, rutaFichero, true);
+                    File.Copy(file, rutaFichero, true);
 
-                Console.WriteLine("Archivo copiado");
+                    Console.WriteLine("Archivo copiado");
+                }
             }
-        }
 
-        Console.WriteLine("\nCopia realizada");
+            Console.WriteLine("\nCopia realizada");
+        }
+        else
+        {
+            Console.WriteLine("La extensión tiene que empezar por un punto.");
+        }
     }
 }
 else
 {
     Console.WriteLine("La ruta introducida no existe.");
 }
-*/
+
 
 Console.WriteLine("-------4-------");
 Console.WriteLine("Introduzca una ruta");
@@ -137,25 +151,13 @@ ruta1 = Console.ReadLine();
 
 if (Directory.Exists(ruta1))
 {
-    nomCarpeta = "";
+    ficheros = Directory.GetFiles(ruta1);
 
-    while (!Regex.IsMatch(nomCarpeta, @"^[\w\d]+$"))
+    Console.WriteLine("Escriba una extensión:");
+    extension = Console.ReadLine();
+    
+    if (extension[0].Equals("."))
     {
-        Console.WriteLine("Introduzca el nombre de una carpeta");
-        nomCarpeta = Console.ReadLine();
-
-        rutaDirectorio = Path.Combine(ruta1, nomCarpeta);
-
-        if (!Directory.Exists(rutaDirectorio))
-        {
-            Directory.CreateDirectory(rutaDirectorio);
-        }
-
-        ficheros = Directory.GetFiles(ruta1);
-
-        Console.WriteLine("Escriba una extensión:");
-        extension = Console.ReadLine();
-
         foreach (String file in ficheros)
         {
             if (extension.ToLower().Equals(Path.GetExtension(file).ToLower()))
@@ -164,18 +166,80 @@ if (Directory.Exists(ruta1))
 
                 nomFichero = rutaSeparada[rutaSeparada.Length - 1];
 
-                rutaFichero = Path.Combine(rutaDirectorio, nomFichero);
-
-                File.Copy(file, rutaFichero, true);
-
-                Console.WriteLine("Archivo copiado");
+                Console.WriteLine("- " + nomFichero);
             }
         }
 
-        Console.WriteLine("\nCopia realizada");
+        Console.WriteLine("¿Desea borrar los archivos? (s/n)");
+        String respuesta = Console.ReadLine();
+
+        if (respuesta.ToLower().Equals("s"))
+        {
+            foreach (String file in ficheros)
+            {
+                if (extension.ToLower().Equals(Path.GetExtension(file).ToLower()))
+                {
+                    File.Delete(file);
+                }
+                else
+                {
+                    rutaSeparada = file.Split("\\");
+
+                    nomFichero = rutaSeparada[rutaSeparada.Length - 1];
+
+                    Console.WriteLine("- " + nomFichero);
+                }
+            }
+        }
+        else if (respuesta.ToLower().Equals("n"))
+        {
+            Console.WriteLine("Vale.");
+        }
+        else
+        {
+            Console.WriteLine("La respuesta introducida es incorrecta.");
+        }
+    }
+    else
+    {
+        Console.WriteLine("La extensión tiene que empezar por un punto.");
     }
 }
 else
 {
     Console.WriteLine("La ruta introducida no existe.");
+}
+
+Console.WriteLine("-------5-------");
+Console.WriteLine("Introduzca una ruta de un fichero");
+ruta1 = Console.ReadLine();
+
+if (File.Exists(ruta1))
+{
+    Console.WriteLine("Escriba la nueva extensión que desea que tenga:");
+    extension = Console.ReadLine();
+
+    if (extension[0].Equals("."))
+    {
+        rutaSeparada = ruta1.Split("\\");
+        nomFichero = rutaSeparada[rutaSeparada.Length - 1];
+
+        String[] nomSeparado = nomFichero.Split(".");
+        String extensionFichero = nomSeparado[nomSeparado.Length - 1];
+        String nomFicheroSinExtension = nomFichero.Substring(0, (nomFichero.Length - extensionFichero.Length - 1));
+        String rutaSinFichero = ruta1.Substring(0, (ruta1.Length - nomFichero.Length));
+        String nomNuevoFichero = nomFicheroSinExtension + extension;
+        String rutaNueva = rutaSinFichero + nomNuevoFichero;
+
+        File.Copy(ruta1, rutaNueva);
+        File.Delete(ruta1);
+    }
+    else
+    {
+        Console.WriteLine("La extensión tiene que empezar por un punto.");
+    }
+}
+else
+{
+    Console.WriteLine("El fichero introducido no existe.");
 }
